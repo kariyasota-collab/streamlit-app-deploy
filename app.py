45ab301f019bb97e99c2e9f6ac208dd119bdd363
kariyasota-collab <kariya.sota@gmail.com>
2026-03-08 22:49:42 +0900

    Rename app.py to app2.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from datetime import datetime
-
-st.set_page_config(page_title="CHECK_8503")
-st.title("CHECK_8503")
-st.write("APP LOADED")
-st.caption(f"loaded from: {__file__}")
-st.caption(f"rendered at: {datetime.now():%Y-%m-%d %H:%M:%S}")
